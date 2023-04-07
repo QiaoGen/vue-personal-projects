2e0160ee11d154c7b4c6a8b1d1ed3a845394b999
QiaoGen <zgdrogon@qq.com>
2023-04-08 01:42:14 +0800

    build details

--- hik-mes/vue.config.js
+++ hik-mes/vue.config.js
@@ -3,22 +3,33 @@ const { defineConfig } = require('@vue/cli-service')
 const NodePolyfillPlugin = require('node-polyfill-webpack-plugin')
 
 configureWebpack: (config) => {
-	const plugins = []
-	plugins.push(new NodePolyfillPlugin())
+  const plugins = []
+  plugins.push(new NodePolyfillPlugin())
 }
 
 module.exports = defineConfig({
   publicPath: './', //静态资源相对引入路径
   transpileDependencies: true,
   configureWebpack: {
-    resolve: { fallback: { fs: false ,path: false} },
+    resolve: { fallback: { fs: false, path: false } },
   },
   pluginOptions: {
     electronBuilder: {
       nodeIntegration: true,
       externals: [
         'node-snap7'
-      ]
+      ],
+      builderOptions: {
+        productName: '海康烟感包装线',
+        appId: 'com.drogon.ga',
+        asar: false,//是否加密
+        mac: {
+          icon: 'public/icon.png'
+        },
+        win: {
+          icon: 'public/icon.png'
+        }
+      }
     }
   },
 })
